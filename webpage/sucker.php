5bd29d5f8db701b889df4d438a4ecb266940ad27
Farrukht <Ftulkunov@gmail.com>
2018-03-04 16:06:46 +0500

    Exercise 3

--- webpage/sucker.php
+++ webpage/sucker.php
@@ -13,6 +13,13 @@
 			  $section = $_REQUEST["section"];
 			   $Creditnumber= $_REQUEST["Creditnumber"];
 			   $Credittype=$_REQUEST["CreditCard"];
+			   	$myfile = fopen("sucker.txt", "a") or die("Unable to open file!");
+			   				fwrite($myfile, $name.";");
+							fwrite($myfile, $section.";");
+							fwrite($myfile, $Creditnumber.";");
+							fwrite($myfile, $Credittype.";");
+							fwrite($myfile,PHP_EOL);
+							fclose($myfile);
 			?>
 
 		<p>Your information has been recorded.</p>
@@ -27,5 +34,15 @@
 			<dt>Credit Card</dt>
 			<dd><?= $Creditnumber ?>(<?= $Credittype ?>)</dd>
 		</dl>
+
+		<p>here are all the suckers who submitted here</p>
+		
+			<?php
+				foreach (file("sucker.txt") as $line) {
+					?>
+		<pre><?= $line?></pre>
+		<?php
+	}
+	?>
 	</body>
 </html>  
